64090fac1983c67692d01356ac16a51f68a6f7ce
bukuzend <bukuzend@gmail.com>
2024-05-11 18:35:07 +0300

    stable version on localhost

--- Helper/getDate.js
+++ Helper/getDate.js
@@ -1,4 +1,4 @@
 module.exports = {
-    getPath:`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().toLocaleTimeString().replaceAll(":","-")}`,
+    getPath:`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}-${new Date().toLocaleTimeString('ru-RU').replaceAll(":","-")}`,
     date: `${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()} ${new Date().toLocaleTimeString()}`
 };
